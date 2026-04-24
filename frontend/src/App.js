484cee73c2be6ab1e41da1f782d4da9f8d9d2acb
deepak <deepak.sh798@email.com>
2026-04-24 14:43:15 +0530

    change frontend ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+"use client";
 import './App.css';
+import { FaDocker } from "react-icons/fa";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Application is Running</h1>
+        <FaDocker size={100} />
+      </div>
+    </>
   );
 }
 
